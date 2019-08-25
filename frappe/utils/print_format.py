e8cb9aabede08c720c7bc03c2dd8810609745c10
Amitabh Akolkar <amitabhakolkar@teamnet.in>
2019-08-25 13:34:42 +0530

    migrate to new server

--- frappe/utils/print_format.py
+++ frappe/utils/print_format.py
@@ -91,6 +91,13 @@ def download_pdf(doctype, name, format=None, doc=None, no_letterhead=0):
 	frappe.local.response.filecontent = get_pdf(html)
 	frappe.local.response.type = "pdf"
 
+@frappe.whitelist(allow_guest=True)
+def download_pdff(doctype, name, format=None, doc=None, no_letterhead=0):
+	html = frappe.get_print(doctype, name, format, doc=doc, no_letterhead=no_letterhead)
+	frappe.local.response.filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
+	frappe.local.response.filecontent = get_pdf(html)
+	frappe.local.response.type = "pdf"
+
 @frappe.whitelist()
 def report_to_pdf(html, orientation="Landscape"):
 	frappe.local.response.filename = "report.pdf"
